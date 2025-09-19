cefbedf70a1ce165c548e0e91bb9c9fe24806799
pulumati <pulumati@realtek.com>
2025-09-19 14:56:19 +0800

    tracepoint subsystems and events lister
    - list enabled tracepoint subsystems
    - list all events within a subsystem

--- 2025/tracepoints-list/src/main.rs
+++ 2025/tracepoints-list/src/main.rs
@@ -1,33 +1,20 @@
 use clap::Parser;
 use colored::*;
 use std::{fs, io};
-// use std::{
-//     fs::{self, File, OpenOptions},
-//     io::{self, BufRead, BufReader, Write},
-//     path::PathBuf,
-//     sync::{
-//         atomic::{AtomicBool, Ordering},
-//         Arc,
-//     },
-// };
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 
 struct Args {
-    // // Name of the person to greet
-    // #[arg(short, long)]
-    // name: String,
-    //
-    // //Number of time to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
-    /// List available events in a subsystem and exit
-    #[arg(long)]
+    #[arg(long, num_args(0..=1), default_missing_value ="all")]
     list: Option<String>,
 }
 
-//fn main() -> io::Result<()> {
 fn main() {
+    println!(
+        "{}{}",
+        "NOTE: Requires ".white().bright_white(),
+        "!!!sudo!!!".red().bright_red().blink()
+    );
     let args = Args::parse();
 
     // --list mode
@@ -36,64 +23,46 @@ fn main() {
         let _x = list_events(subsystem);
     } else {
         // If no subsystem argument is passed  list all subsystem
-        let _x = list_events("");
+        let _x = list_events("all");
     }
 }
 
 const TRACEFS_BASE: &str = "/sys/kernel/debug/tracing";
 
 #[allow(unused_assignments)]
-// fn list_events(subsystem: &str) -> io::Result<()> {
-//     let path = if subsystem.is_empty() {
-//         format!("{}/events", TRACEFS_BASE);
-//     } else {
-//         format!("{}/events/{}", TRACEFS_BASE, subsystem);
-//     };
-//     //let entries = fs::read_dir(&path)?;
-//     println!(
-//         "Available events in {}:",
-//         if subsystem.is_empty() {
-//             "all subsystems"
-//         } else {
-//             subsystem
-//         }
-//     );
-//     let entries = fs::read_dir(&path)?;
-//     for entry in entries {
-//         let entry = entry?;
-//         if entry.file_type()?.is_dir() {
-//             if let Some(name) = entry.file_name().to_str() {
-//                 println!("  {}", name.red());
-//             }
-//         }
-//     }
-//     Ok(())
-// }
 fn list_events(subsystem: &str) -> io::Result<()> {
     // The `if/else` expression returns a String value
     let path = if subsystem.is_empty() {
         format!("{}/events", TRACEFS_BASE)
-    } else if subsystem == "subsystems" {
-        format!("{}/events/{}", TRACEFS_BASE, subsystem)
+    } else if subsystem == "all" {
+        format!("{}/events", TRACEFS_BASE)
     } else {
         format!("{}/events/{}", TRACEFS_BASE, subsystem)
     };
 
     // `path` is guaranteed to be a String, which implements AsRef<Path>
-    println!(
-        "Available events in {}:",
-        if subsystem.is_empty() {
-            "all subsystems"
-        } else {
-            subsystem
-        }
-    );
+    if subsystem == "all" {
+        println!(
+            "{}",
+            "System supported subsystems for tracing: (select the subsystem of interest)".yellow()
+        );
+    } else {
+        println!(
+            "{} {} \n",
+            "Listed tracing events for subsystem:".yellow().underline(),
+            subsystem.bright_white().bold().underline()
+        );
+    }
     let entries = fs::read_dir(&path)?;
     for entry in entries {
         let entry = entry?;
         if entry.file_type()?.is_dir() {
             if let Some(name) = entry.file_name().to_str() {
-                println!("  {}", name.red());
+                if subsystem == "all" {
+                    println!("  {}", name.green().bold());
+                } else {
+                    println!("  {}", name.bright_cyan().bold().italic());
+                }
             }
         }
     }
