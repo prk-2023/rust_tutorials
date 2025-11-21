7d7bae995fd64932511adaa4fea50151d38368e3
pulumati <pulumati@realtek.com>
2025-11-21 17:07:21 +0800

    [update] tracepoint-list program
    - option "--trace-event" to extend  tracepoint list program to dump the
    trace event format.

--- 2025/tracepoints-list/src/main.rs
+++ 2025/tracepoints-list/src/main.rs
@@ -1,12 +1,18 @@
 use clap::Parser;
 use colored::*;
-use std::{fs, io};
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 
 struct Args {
     #[arg(long, num_args(0..=1), default_missing_value ="all")]
     list: Option<String>,
+
+    #[arg(long, num_args(0..=1), help = "event name: dump trace event data format")]
+    trace_event: Option<String>,
 }
 
 fn main() {
@@ -17,13 +23,12 @@ fn main() {
     );
     let args = Args::parse();
 
-    // --list mode
-    if let Some(subsystem) = args.list.as_deref() {
-        //return list_events(subsystem);
-        let _x = list_events(subsystem);
+    let subsystem = args.list.unwrap_or_else(|| "all".to_string());
+
+    if let Some(fmt_x) = args.trace_event {
+        let _x = list_fmt(&subsystem, &fmt_x);
     } else {
-        // If no subsystem argument is passed  list all subsystem
-        let _x = list_events("all");
+        let _x = list_events(&subsystem);
     }
 }
 
@@ -66,5 +71,35 @@ fn list_events(subsystem: &str) -> io::Result<()> {
             }
         }
     }
+    Ok(())
+}
+
+#[allow(unused_assignments)]
+fn list_fmt(subsystem: &str, trace_item: &str) -> io::Result<()> {
+    let file_path = format!(
+        "{}/events/{}/{}/format",
+        TRACEFS_BASE, subsystem, trace_item
+    );
+    println!("DBG:: {file_path}");
+
+    if let Err(e) = read_file_line_by_line(&file_path) {
+        eprintln!("Error reading file: {}", e);
+    }
+    Ok(())
+}
+
+fn read_file_line_by_line(file_path: &str) -> io::Result<()> {
+    // Open the file
+    let file = File::open(file_path)?;
+
+    // Create a buffered reader for the file
+    let reader = BufReader::new(file);
+
+    // Iterate over each line in the file
+    for line in reader.lines() {
+        let line = line?; // Unwrap the Result from the iterator
+        println!("{}", line); // Print each line
+    }
+
     Ok(())
 }
